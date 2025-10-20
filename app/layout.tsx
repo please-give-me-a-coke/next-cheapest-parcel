a0d884bc10c179af2b27059b927d0668098ebb60
please-give-me-a-coke <pleasegivemeacok@gmail.com>
2025-10-21 00:22:38 +0900

    refactor: change from head to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,18 @@ import Header from 'components/header'
 import SystemInfo from 'components/server-info'
 import Footer from 'components/footer'
 import HNCheapestBox from 'components/HNCheapestBox'
+import type { Metadata } from 'next'
 
-export const metadata = {
-  title: 'Cheapest Parcel',
-  description: '택배 가격이 다 달라요~',
-  robots: {
-    index: true,
-    follow: true
+export const metadata: Metadata = {
+  metadataBase: new URL('https://cheapest-parcel.vercel.app'),
+  title: 'Cheapest Parcel - 택배 요금 비교 서비스',
+  description: '편의점 택배 요금을 한눈에 비교하세요.',
+  robots: { index: true, follow: true },
+  openGraph: {
+    type: 'website',
+    url: 'https://cheapest-parcel.vercel.app',
+    title: 'Cheapest Parcel - 택배 요금 비교 서비스',
+    description: '편의점 택배 요금을 한눈에 비교하세요.'
   }
 }
 
